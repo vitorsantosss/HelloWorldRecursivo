161c1785e9f7beba1628b4e407d2e73eefceb882
vitorsantosss <vitor_garcias@uol.com.br>
2014-10-09 02:12:14 -0300

    Boolean para verificar regras básicas de Chomsky

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,24 @@
 import java.util.*;
 public class Main {
-
+	
+	public boolean isChomsky(String [] variaveis, String [] regra){
+		if(variaveis[0].equals("S")) return false;
+		
+		for(int i = 0; i < regra.length; i++) //variavel inicial S nao pode aparecer do lado direito de nenhuma regra
+			if(regra[i].endsWith("S")) return false;
+		
+		for(int i = 1; i < variaveis.length; i++) //somente variavel inicial pode conter regra S->vazio
+			if(variaveis[i].contentEquals("S")) return false;
+		
+		return true;
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
 		String[] linha = sc.nextLine().split(" ");
-		int q = Integer.parseInt(linha[0]);
-		int t = Integer.parseInt(linha[1]);
-		int s = Integer.parseInt(linha[2]);
+		int q = Integer.parseInt(linha[0]); //numero de variaveis
+		int t = Integer.parseInt(linha[1]); //numero de simbolos terminais
+		int s = Integer.parseInt(linha[2]); //numero de regras de substituicao
 		String[] variaveis = sc.nextLine().split(" ");
 		String[] terminais = sc.nextLine().split(" ");
 		Map<String, ArrayList<String>> regras = new HashMap<String, ArrayList<String>>();
